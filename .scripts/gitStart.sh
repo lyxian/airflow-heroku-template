8d12879267c2940b78040b01e68b29484427820a
Lyx <lyxlyxi@hotmail.com>
2021-05-04 02:29:47 +0800

    a

--- .scripts/gitStart.sh
+++ .scripts/gitStart.sh
@@ -5,6 +5,6 @@ git add .
 git commit -m "$whoami-init"
 git push -u origin $branch
 
-# git branch -M main
-# git push heroku main
-# git branch -M $branch
+git branch -M main
+git push heroku main
+git branch -M $branch
