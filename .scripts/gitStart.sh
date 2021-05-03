d186c9f5fc1579f9ab0a3bec735555ed2095de05
Lyx <lyxlyxi@hotmail.com>
2021-05-04 02:24:22 +0800

    -init

--- .scripts/gitStart.sh
+++ .scripts/gitStart.sh
@@ -3,8 +3,8 @@ branch=$(cat whoami)-$(git log --format="%H" -n 1 | head -c 8)
 git checkout -b $branch
 git add .
 git commit -m "$whoami-init"
-git push
+git push -u origin $branch
 
-git branch -M main
-git push heroku main
-git branch -M $branch
+# git branch -M main
+# git push heroku main
+# git branch -M $branch
