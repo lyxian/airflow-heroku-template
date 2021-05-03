7603cc80b25944065211181da7b74dd9d3b91468
Lyx <lyxlyxi@hotmail.com>
2021-05-04 01:43:16 +0800

    init-3.1

--- .scripts/init.sh
+++ .scripts/init.sh
@@ -5,9 +5,6 @@
 x=$(echo "$(python3 --version)" | cut -d " " -f 2 | cut -d "." -f1-2)
 echo $x
 
-### Initialize Virtualenv
-python3 -m venv .venv
-
 ### Create Files/Folders 
 mkdir $PWD/ dags
 mkdir $PWD/.users
@@ -19,25 +16,14 @@ AIRFLOW__CORE__LOAD_EXAMPLES=False
 AIRFLOW__CORE__FERNET_KEY=$(openssl rand -base64 32)
 AIRFLOW__API__AUTH_BACKEND=airflow.api.auth.backend.basic_auth" > .env
 
-echo "# Ignored Files
-.env
-.users" >> .gitignore
-
 ### Install Packages
-source $PWD/.venv/bin/activate
 pip install wheel apache-airflow==2.0.2
 
-### Export variables in .env
-
-set -a
-source $PWD/.env
-set +a
-
 ### Export variables in future sessions
-echo "
-set -a
-source $PWD/.env
-set +a" >> $PWD/.venv/bin/activate
+# echo "
+# set -a
+# source $PWD/.env
+# set +a" >> $PWD/.venv/bin/activate
 
 ### Create Database
 airflow db init
